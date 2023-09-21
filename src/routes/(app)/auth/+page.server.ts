7c8f98c182e0b37ce72c687e4408bb22d3d305c6
rokvisi <rokas.post@gmail.com>
2023-09-21 17:51:46 +0300

    test

--- src/routes/(app)/auth/+page.server.ts
+++ src/routes/(app)/auth/+page.server.ts
@@ -5,21 +5,21 @@ export const actions = {
 	login: async ({ locals, fetch, cookies }) => {
 		console.log('IN LOGIN ACTION');
 
-		const res = await fetch('/api/v1/auth/login', {
-			method: 'POST',
-			body: JSON.stringify(locals.formData)
-		});
+		// const res = await fetch('/api/v1/auth/login', {
+		// 	method: 'POST',
+		// 	body: JSON.stringify(locals.formData)
+		// });
 
-		//* Forward cookies
-		const parsedCookies = cookie.parse(res.headers.getSetCookie()[0]);
-		cookies.set('token', parsedCookies.token, {
-			httpOnly: true,
-			secure: true,
-			...{
-				...parsedCookies,
-				token: undefined
-			}
-		});
+		// //* Forward cookies
+		// const parsedCookies = cookie.parse(res.headers.getSetCookie()[0]);
+		// cookies.set('token', parsedCookies.token, {
+		// 	httpOnly: true,
+		// 	secure: true,
+		// 	...{
+		// 		...parsedCookies,
+		// 		token: undefined
+		// 	}
+		// });
 
 		throw redirect(302, '/');
 	},
