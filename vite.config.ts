c31ed16d38b6be17569b74875aa0567a4dd0ccd8
rokvisi <rokas.post@gmail.com>
2023-09-28 19:00:54 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { loadEnv, defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => {
-	if (mode === 'development') {
-		const env = loadEnv(mode, process.cwd(), '');
-		dotenvExpand.expand({ parsed: env });
-	}
+
 
 	return {
 		plugins: [sveltekit()]
