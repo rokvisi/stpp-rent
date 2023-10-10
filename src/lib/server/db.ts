a0070b5c4bb72bf299be64f48cf8aee43005049a
rokvisi <rokas.post@gmail.com>
2023-10-10 23:45:55 +0300

    add room endpoints and docs

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,15 +1,15 @@
 // Local db connection.
-import postgres from "postgres"
-import { drizzle as drizzlePG } from 'drizzle-orm/postgres-js';
-import * as schema from '$lib/database/schema';
-
-const db = drizzlePG(postgres(process.env.PRIVATE_POSTGRES_URL_LOCAL as string), { schema });
-export default db;
-
-// Deployment db connection.
-// import { sql } from '@vercel/postgres';
-// import { drizzle as drizzleVercelPG } from 'drizzle-orm/vercel-postgres';
+// import postgres from "postgres"
+// import { drizzle as drizzlePG } from 'drizzle-orm/postgres-js';
 // import * as schema from '$lib/database/schema';
 
-// const db = drizzleVercelPG(sql, { schema })
+// const db = drizzlePG(postgres(process.env.PRIVATE_POSTGRES_URL_LOCAL as string), { schema });
 // export default db;
+
+// Deployment db connection.
+import { sql } from '@vercel/postgres';
+import { drizzle as drizzleVercelPG } from 'drizzle-orm/vercel-postgres';
+import * as schema from '$lib/database/schema';
+
+const db = drizzleVercelPG(sql, { schema })
+export default db;
